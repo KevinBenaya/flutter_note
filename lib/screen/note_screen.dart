3f8d06c1b1f229debd958965d505ed1ec54acd92
KevinBenaya <darkfaust063@gmail.com>
2022-05-28 09:09:19 +0700

    NoteScreenWidget

--- lib/screen/note_screen.dart
+++ lib/screen/note_screen.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_note/models/models.dart';
+import 'package:flutter_note/widgets/widgets.dart';
 
 class NoteScreen extends StatelessWidget {
   const NoteScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddScreen(),
+            ),
+          );
+        },
+        child: Icon(Icons.add, size: 30, color: Colors.blueGrey),
+        backgroundColor: Colors.white,
+      ),
+      appBar: AppBar(
+        title: Text(
+          'Task Manager',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Consumer<NotesOperation>(
+        builder: (context, NotesOperation data, child) {
+          return ListView.builder(
+            itemCount: data.getNotes.length,
+            itemBuilder: (context, index) {
+              return NotesCard(data.getNotes[index]);
+            },
+          );
+        },
+      ),
+    );
   }
 }
