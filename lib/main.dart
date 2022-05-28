41edbe995a5b1f3dc2bfc5b7b1a861b090405402
KevinBenaya <darkfaust063@gmail.com>
2022-05-28 08:45:52 +0700

    Note

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Note());
+}
+
+class Note extends StatelessWidget {
+  const Note({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
