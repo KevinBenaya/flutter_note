d18ed1dedff88f3e77e73e24a435000fb91e566d
KevinBenaya <darkfaust063@gmail.com>
2022-05-28 09:10:43 +0700

    Fixing some problems

--- lib/widgets/notes_card_widget.dart
+++ lib/widgets/notes_card_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_note/models/models.dart';
-import 'package:flutter_note/screen/note_screen.dart';
 
 class NotesCard extends StatelessWidget {
   final Note note;
@@ -10,8 +9,8 @@ class NotesCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(15),
-      padding: EdgeInsets.all(15),
+      margin: const EdgeInsets.all(15),
+      padding: const EdgeInsets.all(15),
       height: 150,
       decoration: BoxDecoration(
         color: Colors.white,
@@ -22,12 +21,12 @@ class NotesCard extends StatelessWidget {
         children: [
           Text(
             note.title,
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-          SizedBox(height: 5),
+          const SizedBox(height: 5),
           Text(
             note.description,
-            style: TextStyle(fontSize: 17),
+            style: const TextStyle(fontSize: 17),
           ),
         ],
       ),
