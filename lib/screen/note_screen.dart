b440dd58803a3c9964d59d56ecb5d1304621f5da
KevinBenaya <darkfaust063@gmail.com>
2022-05-28 09:01:03 +0700

    Adding note_screen.dart

---
+++ lib/screen/note_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_note/models/models.dart';
+
+class NoteScreen extends StatelessWidget {
+  const NoteScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
