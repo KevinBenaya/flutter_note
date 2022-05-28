e37ddd90fe968acfa4ed1d6600daeb8d18c7824a
KevinBenaya <darkfaust063@gmail.com>
2022-05-28 16:23:36 +0700

    Editing add_screen.dart

--- lib/screen/add_screen.dart
+++ lib/screen/add_screen.dart
@@ -1,90 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_note/models/models.dart';
-import 'package:flutter_note/screen/screeen.dart';
 
 class AddScreen extends StatelessWidget {
+  /*@override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AddScreen(),
+    );
+  }*/
+
+  const AddScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    String judulTeks;
-    String ddeskripsiTeks;
+    String? judulTeks;
+    String? deskripsiTeks;
 
     return Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        title: Text('KelasKu'),
+        title: const Text('KelasKu'),
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      body: Padding(
-        padding: EdgeInsets.only(
-          top: 15,
-          left: 15,
-          right: 15,
-          bottom: 80,
-        ),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Judul',
-                hintStyle: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-              onChanged: (value) {
-                judulTeks = value;
-              },
-            ),
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.4,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 15,
+            left: 15,
+            right: 15,
+            bottom: 80,
+          ),
+          child: Column(
+            children: [
+              TextField(
+                decoration: const InputDecoration(
                   border: InputBorder.none,
-                  hintText: 'Enter Description',
+                  hintText: 'Judul',
                   hintStyle: TextStyle(
-                    fontSize: 18,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                     color: Colors.white,
                   ),
                 ),
-                style: TextStyle(
-                  fontSize: 18,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                   color: Colors.white,
                 ),
                 onChanged: (value) {
-                  ddeskripsiTeks = value;
+                  judulTeks = value;
                 },
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                Provider.of<NotesOperation>(context, listen: false)
-                    .addNewNote(judulTeks, ddeskripsiTeks);
-                Navigator.pop(context);
-              },
-              style: TextButton.styleFrom(
-                padding:
-                    EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 10),
-                backgroundColor: Colors.white,
-              ),
-              child: Text(
-                'Tambahkan Catatan',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueGrey,
+              Expanded(
+                child: TextField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Enter Description',
+                    hintStyle: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                  onChanged: (value) {
+                    deskripsiTeks = value;
+                  },
                 ),
               ),
-            )
-          ],
+              TextButton(
+                onPressed: () {
+                  Provider.of<NotesOperation>(context, listen: false)
+                      .addNewNote(judulTeks!, deskripsiTeks!);
+                  Navigator.pop(context);
+                },
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.only(
+                      left: 30, right: 30, top: 10, bottom: 10),
+                  backgroundColor: Colors.white,
+                ),
+                child: const Text(
+                  'Tambahkan Catatan',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
