358be950f58bc15b495c2ef0c47ec52e761d6c9e
KevinBenaya <darkfaust063@gmail.com>
2022-05-28 16:23:15 +0700

    Editing main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_note/models/models.dart';
+import 'package:flutter_note/screen/screeen.dart';
 
-void main() {
-  runApp(const Note());
-}
+void main() => runApp(const Note());
 
 class Note extends StatelessWidget {
   const Note({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return ChangeNotifierProvider<NotesOperation>(
+      create: (context) => NotesOperation(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: NoteScreen(),
+      ),
+    );
   }
 }
